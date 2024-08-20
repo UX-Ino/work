0747b031d3caf9b4814788ab081430c6b071f9f5
정인호 <61679902+UX-Ino@users.noreply.github.com>
2024-08-20 14:22:55 +0900

    2222

--- uxg-boilerplate-2024-dev-a38668c1546ae5ae7f6f4d34e61dc4ea16872d07/eslint.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = [
-  {
-    files: ["src/assets/js/**/*.js", "src/assets/js/**/*.cjs"],
-    rules: {
-      "quotes": ["error", "single"],
-      "prefer-const": "error",
-    },
-  },
-]
--- uxg-boilerplate-2024-dev-a38668c1546ae5ae7f6f4d34e61dc4ea16872d07/src/assets/scripts/common.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  console.log('DOMContentLoaded');
-  initUI();
-
-
-
-})
